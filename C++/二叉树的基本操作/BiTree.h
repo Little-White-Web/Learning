bffdfa02f5856c2f4c7901e9052f220aa6378c64
Little-White-Web <57803679+Little-White-Web@users.noreply.github.com>
2019-11-22 13:21:34 +0800

    Update BiTree.h

--- C++/二叉树的基本操作/BiTree.h
+++ C++/二叉树的基本操作/BiTree.h
@@ -1,14 +1,14 @@
 #define TRUE 1
-#define FALSE -1
+#define FALSE 0
 #define OK 1
-#define ERROR -1
+#define ERROR 0
 
 typedef struct BiTNode{
-	int data;
+	char data;
 	struct BiTNode *lchild, *rchild;	//左右孩子指针
 }BiTNode, *BiTree;
 
-int CreateBiTree(Bitree &T);			//操作函数的声明
-int PreOrderTraverse(BiTree T,int (*Visit)(int e));
-int InOrderTraverse(BiTree T,int (*Visit)(int e));
-int PrintElement(int e);
+int CreateBiTree(BiTree &T);			//二叉树操作函数声明
+int PreOrderTraverse(BiTree T,int (*Visit)(char e));
+int InOrderTraverse(BiTree T,int (*Visit)(char e));
+int PrintElement(char e);
