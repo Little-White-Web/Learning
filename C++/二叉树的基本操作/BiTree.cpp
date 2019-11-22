1aef1c48c82f85c065eae63f41b93d43f9dcbd6b
Little-White-Web <57803679+Little-White-Web@users.noreply.github.com>
2019-11-22 17:48:26 +0800

    Update BiTree.cpp

--- C++/二叉树的基本操作/BiTree.cpp
+++ C++/二叉树的基本操作/BiTree.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int CreateBiTree(BiTree &T){		//按先序构造二叉树
 	char ch;
-	printf("请输入数据：");
 	scanf("%c",&ch);
-	getchar();				//抵消回车符
 	if(ch=='*') T=NULL;
 	else{
 		if( !(T=(BiTree)malloc(sizeof(BiTNode))) ) exit(FALSE);
@@ -40,7 +38,40 @@ int InOrderTraverse(BiTree T,int (*Visit)(char e)){		//中序遍历
 	return OK;
 }
 
-int PrintElement(char e){			//打印元素
+int PostOrderTraverse(BiTree T,int (*Visit)(char e)){		//后序遍历
+	if(T){
+		if(PostOrderTraverse(T->lchild,Visit)){	//遍历左子树
+			if(PostOrderTraverse(T->rchild,Visit)){	//遍历右子树
+				if(Visit(T->data)) return OK;	//访问根节点
+			}
+		}
+	}
+	return OK;
+}
+
+int TreeDeep(BiTree T){		//求树的深度
+	int deep=0;				//初始深度为0
+	if(T){
+		int ldeep=TreeDeep(T->lchild);	//求左子树深度
+		int rdeep=TreeDeep(T->rchild);	//求右子树深度
+		deep = ldeep>=rdeep ? ldeep+1:rdeep+1;	//将较大的深度+1赋给根节点的深度
+	}
+	return deep;
+}
+
+int LeafCount(BiTree T){	//求叶子节点个数
+	static int count=0;		//初始个数为0
+	if(T){
+		if(!T->lchild && !T->rchild){
+			count++;		//左右孩子都为空（即叶子）计数加一
+		}
+		LeafCount(T->lchild);	//访问左节点
+		LeafCount(T->rchild);	//访问右节点
+	}
+	return count;
+}
+
+int PrintElement(char e){			//打印元素(Visit函数)
 	printf("%c\t",e);
 	return OK;
 }
