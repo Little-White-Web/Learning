eb2b3ea58cc9097e2831110c6068eade9cab039b
Little-White-Web <57803679+Little-White-Web@users.noreply.github.com>
2019-11-24 14:50:56 +0800

    Update Main.cpp

--- C++/二叉树的基本操作/Main.cpp
+++ C++/二叉树的基本操作/Main.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(){
-	BiTree T;			//生成一颗二叉树
+	BiTree T;				//生成一颗二叉树
 	printf("先序创建二叉树，输入*结束叶子节点的输入！\n>>");
-	CreateBiTree(T);	//构造（初始化）这颗二叉树
+	CreateBiTree(T);			//构造（初始化）这颗二叉树
 
 	printf("\n先序遍历：\n");
 	PreOrderTraverse(T,PrintElement);	//先序遍历
